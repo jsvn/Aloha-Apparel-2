450be494ea00c5ee8af6e6f3feefcb564593e07b
jasveen <jsvn.gill@gmail.com>
2017-01-23 00:02:11 -0800

    project-01b

--- js/main.js
+++ js/main.js
@@ -1,8 +1,49 @@
-$('#my-form').on('submit', function(event) {
-   event.preventDefault();
-   if ( $('#my-text-input').val() == '' ) {
-      alert('You missed the field.');
-   } else {
-      alert('Thanks for filling the field!');
-   }
+// Add to Cart
+		var totalItems = 0; //set initial to 0
+		$("button").click(function(){ //when button is clicked, func runs
+			$("div.total").css({"display":"flex"}); // shows the counter at the top
+			totalItems = Number(totalItems) + Number($(this).val()); // .val() brings in the value which is 1 and adds it to the totalItems
+			$(".total").text(totalItems);
+		});
+
+// Smooth Scroll
+		$('a[href^="#"]').on("click",function (e) {
+		    e.preventDefault();
+
+		    var target = this.hash;
+		    var $target = $(target);
+
+		    $("html, body").stop().animate({
+		        "scrollTop": $target.offset().top
+		    }, 600, "swing", function () { //speed for scroll is here in this line
+		        window.location.hash = target;
+		    });
+		});
+
+// Slider
+$(function(){
+
+    $(".products").flickity({
+           cellAlign: 'right',
+        contain: true,
+    });
 });
+
+// Email validator
+
+$('.sign-up-email').submit( function(){
+    return false;
+})
+
+})
+
+
+// $( "form" ).submit(function( event ) {
+//   if ( $( "input:first" ).val() === “email“ ) {
+//     $( "span" ).text( "Validated..." ).show();
+//     return;
+//   }
+//
+//   $( "span" ).text( “Please submit valid email address” ).show().fadeOut( 1000 );
+//   event.preventDefault();
+// });
