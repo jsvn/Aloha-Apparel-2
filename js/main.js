e0bd5d0e729848b5bba6aa4aab14494d39813a70
jasveen <jsvn.gill@gmail.com>
2017-01-23 04:20:31 -0800

    First commit

--- js/main.js
+++ js/main.js
@@ -6,56 +6,26 @@
 			$(".total").text(totalItems);
 		});
 
-    $(function() {
-        $('.products').flickity({
-            cellAlign: 'center',
-            contain: true,
-            percentPosition: true,
-            imagesLoaded: true,
-            autoPlay: true,
-            prevNextButtons: false
-        });
-})
+//     $(function() {
+//         $('.products').flickity({
+//             cellAlign: 'center',
+//             contain: true,
+//             percentPosition: true,
+//             imagesLoaded: true,
+//             autoPlay: true,
+//             prevNextButtons: false
+//         });
+// })
 
-// // Smooth Scroll
-// 		$('a[href^="#"]').on("click",function (e) {
-// 		    e.preventDefault();
-//
-// 		    var target = this.hash;
-// 		    var $target = $(target);
-//
-// 		    $("html, body").stop().animate({
-// 		        "scrollTop": $target.offset().top
-// 		    }, 600, "swing", function () { //speed for scroll is here in this line
-// 		        window.location.hash = target;
-// 		    });
-// 		});
 
-    //smooth scrolling
+Slider
+$(function(){
 
-// $(function() {
-//   $('a[href*="#"]:not([href="#"])').click(function() {
-//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-//       var target = $(this.hash);
-//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-//       if (target.length) {
-//         $('html, body').animate({
-//           scrollTop: target.offset().top
-//         }, 1000);
-//         return false;
-//       }
-//     }
-//   });
-// });
-
-// Slider
-// $(function(){
-//
-//     $(".products").flickity({
-//            cellAlign: 'right',
-//         contain: true,
-//     });
-// });
+    $(".products").flickity({
+           cellAlign: 'right',
+        contain: true,
+    });
+});
 
 // Email validator
 
